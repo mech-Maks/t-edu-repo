e8e7e8fa2c90c7931cba2a33827e307edb4ad136
m.deryugin <m.deryugin@tinkoff.ru>
2023-09-10 17:17:27 +0300

    init commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,14 @@
 package org.example;
 
+import java.util.*;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        System.out.println("hey yo hello mir");
     }
 }
